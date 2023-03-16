c5d4d1290acec3ad98b1ae211de265d58775b08a
josepsr <josepsr@ext.inditex.com>
2023-03-16 09:35:33 +0100

    move ports to domain

---
+++ src/main/java/gl/js/jhex/domain/inputport/CustomerInputPort.java
@@ -0,0 +1,15 @@
+package gl.js.jhex.domain.inputport;
+
+import java.util.List;
+
+import gl.js.jhex.domain.Customer;
+
+public interface CustomerInputPort {
+
+  public Customer createCustomer(String name, String country);
+
+  public Customer getById(String customerId);
+
+  public List<Customer> getAll();
+
+}
